139fc5ae1efa82fddd683b980b33ebff3f7dbf36
Pawfa <fajfer.pawel@gmail.com>
2018-06-03 20:29:38 +0200

    Minor corrections.

--- backend/services/externalApiMoviesFetchingService.js
+++ backend/services/externalApiMoviesFetchingService.js
@@ -1,5 +1,5 @@
 let rp = require('request-promise');
 module.exports.fetch = function(searchQuery) {
-  return rp('http://www.omdbapi.com/?apikey=f907da9a&t='+searchQuery);
+  return rp('http://www.omdbapi.com/?apikey=f907da9a&t='+searchQuery.replace(" ","+"));
 };
 
