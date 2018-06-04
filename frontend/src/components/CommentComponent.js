e8aa697f19994152d34f38a2895db163889ca0c1
Pawfa <fajfer.pawel@gmail.com>
2018-06-04 07:00:19 +0200

    Minor corrections.

--- frontend/src/components/CommentComponent.js
+++ frontend/src/components/CommentComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './CommentComponent.css';
+import {Fa} from "mdbreact";
 
 class CommentComponent extends Component {
 
@@ -9,13 +10,15 @@ class CommentComponent extends Component {
     const utc = date.getTime();
     const currDate = new Date(utc);
     return <div className={'commentComponent z-depth-1'}>
-      <header className="teal-text"><h6 className="font-weight-bold mb-3">
-        <span className="fa fa-calendar pr-2"/></h6>{currDate.toDateString()} {currDate.toLocaleTimeString()}</header>
+
+      <header className="teal-text">
+        <h6 className="font-weight-bold mb-3">
+          <Fa className="fa fa-calendar pr-2"/>{currDate.toDateString()} {currDate.toLocaleTimeString()}</h6>
+        </header>
       <p className={'commentComponent-content z-depth-1'}>{content}</p>
     </div>;
   }
 
 }
-
 
 export default CommentComponent;
